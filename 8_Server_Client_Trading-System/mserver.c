77315843455f902a7c9f8cab993eba5bb33adc21
DeepankarAcharyya <deepankaracharyya@gmail.com>
2019-11-30 19:42:24 +0530

    started with 8

---
+++ 8_Server_Client_Trading-System/mserver.c
@@ -0,0 +1,39 @@
+//server program
+
+//include libraries
+#include "string.h"
+#include "server.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include "stdio.h"
+#include <stdlib.h>
+#include "netdb.h"
+#include "netinet/in.h"
+
+//the authorisation function
+int authorised( char *user, char *password){
+
+    FILE *fp = fopen("login.txt", "r");
+    char buff[50];
+    
+    while( fgets(  buff, sizeof(buff), fp) != NULL ){
+        char *user1 = strtok(buff, ":");
+        char *pass1 = strtok(NULL, "\n");
+        
+        if( strcmp( user, user1) == 0 ){
+            if( strcmp(password, pass1)==0)
+               return 0; //matched
+            else 
+               return 1; //wrong password
+        }
+    }
+    return -1; //user not found
+}
+
+
+//main function
+int main(int argc,char** argv){
+
+
+    return 0;
+}
