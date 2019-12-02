588d9df99a79067d0dd70a3dc9211dd593a401db
DeepankarAcharyya <deepankaracharyya@gmail.com>
2019-12-02 23:49:08 +0530

    p1

--- 8_Server_Client_Trading-System/mserver.c
+++ 8_Server_Client_Trading-System/mserver.c
@@ -42,8 +42,67 @@ int authorised( char *user, char *password){
 */
 
 //main function
-int main(int argc,char** argv){
+int main(int argc, char const *argv[]){
+//extracting the input values from the command line arguments
+char PORT[10], server_ip[16];
+
+if(argc<3){
+    printf("\nInsufficient arguments!\n");
+    exit(1);
+}
+else{
+    strcpy(PORT,argv[2]);
+    strcpy(server_ip,argv[1]);
+}
+
+//socket structure declaration and initializing it to null
+struct  sockaddr_in server_add;
+bzero((char *)&server_add,sizeof(server_add));
+server_add.sin_family=AF_INET;
+server_add.sin_port=htons(atoi(PORT));
+server_add.sin_addr.s_addr=inet_addr(server_ip);
 
 
+//Socket Creation
+int socket_server = socket(AF_INET, SOCK_STREAM, 0);
+
+if(socket_server<0){
+    perror("\n Error in creating/opening the Socket!\n");
+    exit(1);
+}
+else{
+    printf("\nSocket Created Successfully!\n");
+}
+
+//Binding the socket 
+int bind_value=bind(socket_server,(struct sockaddr*)&server_add,sizeof(server_add));
+
+if(bind_value<0){
+    perror("\n Error in Socket Binding!\n");
+    exit(1);
+}
+else{
+    printf("\nSocket Binded Successfully!\n");
+}
+
+//Listening for incoming requests
+listen(socket_server,5);
+
+//accepting the incoming request
+int client_socket=accept(socket_server,NULL,NULL);
+
+
+if(client_socket<0){
+    perror("\n Error in accepting connection!\n");
+    exit(1);
+}
+else{
+    printf("\nConnection Accepted Successfully!\n");
+    //do the needfull
+}
+
+//Close the socket
+close(socket_server);
+printf("\nclosed the socket.\n");
     return 0;
 }
