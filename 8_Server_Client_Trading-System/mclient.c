b3360b3310ae0034564ed1bafc3020d0ca289d7e
DeepankarAcharyya <deepankaracharyya@gmail.com>
2019-12-03 21:17:53 +0530

    p++

---
+++ 8_Server_Client_Trading-System/mclient.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    //connecting to the server part
+
+    //login part
+    int trader_id;
+    printf("Enter the trade ID:");
+    scanf("%d",&trader_id);
+
+    return 0;
+}
