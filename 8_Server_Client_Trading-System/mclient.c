03e7b298469eb30a0bd064b4081347e610512b5e
DeepankarAcharyya <deepankaracharyya@gmail.com>
2019-12-04 13:56:12 +0530

    p++

--- 8_Server_Client_Trading-System/mclient.c
+++ 8_Server_Client_Trading-System/mclient.c
@@ -4,6 +4,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+void display_menu(){
+    printf("\n**********************MENU*************************\n");
+    printf("\nEnter 1 for Buying\n");
+    printf("\nEnter 2 for Selling\n");
+    printf("\nEnter 3 for Viewing Order Stats\n");
+    printf("\nEnter 4 for Viewing the available stocks to Buy\n");
+    printf("\n***************************************************\n");
+}
 int main(){
     //connecting to the server part
 
@@ -12,5 +20,13 @@ int main(){
     printf("Enter the trade ID:");
     scanf("%d",&trader_id);
 
+    //displaying the menu
+
+    //taking input
+
+    //if request based-> filling up the request structure 
+    
+    //sending the request structure to the server
+    
     return 0;
 }
